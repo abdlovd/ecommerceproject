f7221433cf4729f70b719bbe46410c61cc6f6c10
Your Name <dorkinabdulova@gmail.com>
2026-01-26 17:25:16 +0400

    functions for homework 14.2 was written with tests

--- src/task.py
+++ src/task.py
@@ -7,8 +7,34 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, name, description, price, quantity, products):
+        for product in products:
+            if product.name == name:
+                product.quantity += quantity
+                if price > product.price:
+                    product.price = price
+                return product
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if self.__price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif self.__price > new_price:
+            print(f"Подтверждаете понижение цены? Введите 'y' если да, 'n' если нет")
+            answer = input()
+            if answer == "y":
+                self.__price = new_price
+            return
+        self.__price = new_price
 
 class Category:
     name: str
@@ -20,6 +46,20 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.total_categories += 1
         Category.total_products += len(products)
+
+    def add_product(self, name, description, price, quantity):
+        self.__products.append(Product(name, description, price, quantity))
+
+    @property
+    def products(self):
+        return self.__products
+
+    @property
+    def products_ (self):
+        products_str = " "
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
--- tests/test_task.py
+++ tests/test_task.py
@@ -1,3 +1,6 @@
+from src.task import Product
+
+
 def test_task_product(product) -> None:
     assert product.name == "Product 1"
     assert product.description == "Product 2"
@@ -8,4 +11,18 @@ def test_task_category(category) -> None:
     assert category.name == "Product 3"
     assert category.description == "Product 4"
     assert category.products == "Product 5"
-    
+
+def test_add_products(category) -> None:
+    assert category.products == 'Product 5'
+
+def test_new_products():
+    new_product = Product(name="Product 1", description="Product 2", price="100", quantity=10)
+    new_product.name = "Product 1"
+    new_product.description = "Product 2"
+    new_product.price = "100"
+    new_product.quantity = 10
+
+def test_price(capsys, product):
+    product.price = "100"
+    message = capsys.readouterr()
+    assert message.out.strip () == "Цена не должна быть нулевая или отрицательная"
